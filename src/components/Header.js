2fe27b8007c5cc13bcc9b6a48bbb792bbf80aa9e
zarazi <zarazi@hotmail.com>
2016-12-27 20:04:09 +0700

    Initial repo

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+export default() => (
+  <div className="App-header">
+    <img src={logo} className="App-logo" alt="logo"/>
+    <h2>Welcome to React</h2>
+  </div>
+);
